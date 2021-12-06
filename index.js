66644cccb8e6232703f9849d3e0dd1e0e220ec0a
Tech-Nik-Hub <55291481+Tech-Nik-Hub@users.noreply.github.com>
2021-12-06 21:46:44 +0530

    added controllers and setup ejs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const express=require('express');
-const port=3000;
+
 const app=express();
+const port=3000;
+
+//using express router
+app.use('/',require('./routes/index'));
+
+app.set('view engine','ejs');
+
+app.set('views','./views');
 
 
 app.listen(port,function(err){
