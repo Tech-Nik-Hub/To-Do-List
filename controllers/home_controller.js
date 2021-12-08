75f009b79782447c497873866b832b5bb86f5b3e
Tech-Nik-Hub <55291481+Tech-Nik-Hub@users.noreply.github.com>
2021-12-08 13:31:48 +0530

    made basic list and css

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,18 @@
+var tasksList=[
+    {
+        name:"Drink water",
+    },
+    {
+        name:"Drink coffee",
+    }
+];
+
+
+
 module.exports.home=function(req,res){
     return res.render('home',{
         title:"To-Do List",
+        tasks:tasksList,
     });
 }
 
--- index.js
+++ index.js
@@ -6,12 +6,27 @@ const port=3000;
 const db=require('./config/mongoose');
 //using express router
 app.use('/',require('./routes/index'));
-
+//setting up view engine
 app.set('view engine','ejs');
 
 app.set('views','./views');
 
+//setting up assets to use
+
 app.use(express.static('assets'));
+
+
+
+var tasksList=[
+    {
+        name:"Drink water"
+    },
+    {
+        name:"Drink coffee"
+    }
+];
+
+
 
 
 app.listen(port,function(err){
